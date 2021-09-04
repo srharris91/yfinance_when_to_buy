3f2c17a95fa540fd86d8162b7a5d9be682e23768
Shaun Harris <shaun.r.harris@gmail.com>
2021-09-03 18:03:23 -0600

    Tested backtrace for best case scenario one trade a day and attempted to train NN to predict this buy,hold,sell class

--- backtrace_best.py
+++ backtrace_best.py
@@ -10,9 +10,8 @@ plt.style.use('seaborn-notebook')
 today = date.today()
 dend = today.isoformat()
 #print(dstart,dend)
-from_days_back = 1000 # earliest day to test 
-to_days_back = 1 # latest day to test, 1 means yesterday
-dstart = (today-timedelta(days=from_days_back+600)).isoformat() # 60 days prior to today
+from_days_back = 30 # earliest day to test 
+dstart = (today-timedelta(days=from_days_back)).isoformat() # 60 days prior to today
  
 ticker = 'TQQQ'
 history = yf.Ticker(ticker).history(start=dstart,interval='1d') # should give 280 rows of data (40 work days, 7 hours a day)
@@ -33,18 +32,20 @@ for tick in axtick.get_xticklabels():
 
 # now model it like each day, starting amount is $10,000, assume no fees on transactions
 def model_growth_of_best(data):
-    net_worth = pd.DataFrame({'net worth':np.zeros(999), 'strategy':np.zeros(999)},index=data.index[-from_days_back:-to_days_back])
+    num_days = data.shape[0]
+    net_worth = pd.DataFrame({'net worth':np.zeros(num_days), 'strategy':np.zeros(num_days)},index=data.index)
     net_worth['strategy'] = net_worth['strategy'].astype(str)
-    funds = 10000.00
+    funds = 1000.00
     #price = data['Close'][-from_days_back-1] # day to purchase
     #funds -= price # purchase one share at the beginning
     shares = 0
-    for day,i in enumerate(range(-from_days_back,-to_days_back-2)):
-        d = data[i-1:i+2]
-        what_to_do = 'hold'
+    for i,day in enumerate(data.index[:-1]):
         if i==0:
             what_to_do = 'buy'
+            price = data['Close'][i]
         else:
+            d = data[i-1:i+2]
+            what_to_do = 'hold'
             argmax = np.argmax(d['Close'])
             argmin = np.argmin(d['Close'])
             if argmax==1:
@@ -53,21 +54,22 @@ def model_growth_of_best(data):
                 what_to_do = 'buy'
             else:
                 what_to_do = 'hold'
-        price = d['Close'][1]
+            price = d['Close'][1]
         if what_to_do == 'buy':
-            funds -= price
-            shares += 1
+            num_shares_to_buy = funds//price
+            funds -= num_shares_to_buy*price
+            shares += num_shares_to_buy
         elif what_to_do == 'sell':
-            funds += price
-            shares -= 1
+            funds += shares*price
+            shares = 0
         elif what_to_do == 'hold':
             pass
-        net_worth.loc[net_worth.index[day],'net worth'] = funds + price*shares
-        net_worth.loc[net_worth.index[day],'strategy'] = what_to_do
-    for day,i in enumerate(range(-2,0)):
-        price = data['Close'][i]
-        net_worth.loc[net_worth.index[i],'net worth'] = funds + price*shares
-        net_worth.loc[net_worth.index[i],'strategy'] = 'hold'
+        net_worth.loc[day,'net worth'] = funds + price*shares
+        net_worth.loc[day,'strategy'] = what_to_do
+    for i,day in enumerate(data.index[-1:]):
+        price = data.loc[day,'Close']
+        net_worth.loc[day,'net worth'] = funds + price*shares
+        net_worth.loc[day,'strategy'] = 'hold'
     # sell at the end
     print('selling shares at the end')
     price = data['Close'][-1]
@@ -83,12 +85,12 @@ net_worth_best = model_growth_of_best(history)
 fig,ax = plt.subplots(figsize=(4.75,3),dpi=200,tight_layout=True)
 ax.plot(net_worth_best['net worth'],label='best')
 def plot_buy_and_sell(net_worth,ax,column='net worth'):
-    for i in range(-from_days_back+1,0):
+    for i,day in enumerate(net_worth.index):
         if net_worth['strategy'][i] == 'buy':
             ax.plot(net_worth.index[i],net_worth[column][i],'g^')
         elif net_worth['strategy'][i] == 'sell':
             ax.plot(net_worth.index[i],net_worth[column][i],'rv')
-net_worth_best['Close'] = history['Close'][-from_days_back:-to_days_back]
+net_worth_best['Close'] = history['Close']
 plot_buy_and_sell(net_worth_best,axtick,column='Close')
 ax.set_xlabel('days')
 ax.set_ylabel('net worth $')
--- predict_future_NN2.py
+++ predict_future_NN2.py
@@ -13,7 +13,7 @@ plt.style.use('seaborn-notebook')
 
 today = date.today()
 dend = today.isoformat()
-dstart = (today-timedelta(days=2000)).isoformat() # 60 days prior to today
+dstart = (today-timedelta(days=10000)).isoformat() # 60 days prior to today
 #print(dstart,dend)
 
 ticker = 'TQQQ'
@@ -37,23 +37,33 @@ for i,closei in enumerate(labels['Close']):
     else:
         labels.loc[labels.index[i],'class'] = 1 # hold if first day
 
-history_train = pd.DataFrame(history[:-100])
-labels_train = pd.DataFrame(labels[:-100])
-history_val = pd.DataFrame(history[-100:])
-labels_val = pd.DataFrame(labels[-100:])
+perm = np.random.permutation(history.shape[0]-21)+20
+#print(history.shape,history.loc[history.index[[2,3,4]]])
+#history_train = pd.DataFrame(history.loc[history.index[perm[:-100]]])
+#history_val = pd.DataFrame(history.loc[history.index[perm[-100:]]])
+#print(history_train)
 
 
-stock = StockDataFrame.retype(history_train)
-image_list = ['rsi_6','rsi_12','wr_10','wr_6','macd','close_6_ema','close_6_sma','trix','tema','cci','cci_20','kdjk','dma','pdi','mdi','dx','adx','adxr','cr','tr','atr','boll']
+stock = StockDataFrame.retype(history)
+#image_list = ['rsi_6','rsi_12','wr_10','wr_6','macd','close_6_ema','close_6_sma','trix','tema','cci','cci_20','kdjk','dma','pdi','mdi','dx','adx','adxr','cr','tr','atr','boll']
+#image_list = ['macd','tema','dma','adx','boll']
+#image_list = ['rsi_6','rsi_12','wr_10','wr_6','macd','macdh','close_6_ema','close_20_ema','close_6_sma','close_12_sma','high_12_ema','high_6_ema','high_12_sma','high_6_sma','low_6_ema','low_12_ema','low_6_sma','low_12_sma','trix','trix_9_sma','tema','cci','cci_20','kdjk','kdjd','kdjj','dma','pdi','mdi','dx','adx','adxr','cr','tr','atr','boll','volume_delta','volume_-2_d','volume_-3_d','volume_-4_d','open_delta','open_-2_d','open_-3_d','open_-4_d','high_delta','high_-2_d','high_-3_d','high_-4_d','close_delta','close_-2_d','close_-3_d','close_-4_d','low_delta','low_-2_d','low_-3_d','low_-4_d','close_-1_r','close_-2_r','close_-3_r','vr','vr_6_ema']
+#image_list = ['rsi_6','wr_10','macd','close_6_ema','close_20_ema','high_20_ema','high_6_ema','low_6_ema','low_20_ema','trix','tema','cci','kdjk','dma','pdi','mdi','dx','adx','cr','tr','atr','boll','volume_delta','high_delta','close_delta','low_delta'] # H=10 works well
+#image_list = ['rsi_6','wr_10','macd','close_6_ema','high_6_ema','low_6_ema','tema','dma','boll','volume_delta','high_delta','close_delta','low_delta']
+#image_list = ['boll','volume_-1_d','close_-1_d','close_6_ema'] # works okay with H=10
+image_list = ['boll','volume_-1_d','close_-1_d'] # works okay with H=10
 stock[image_list] # calculate statistics
+print(stock)
+print(stock.columns)
+stock_train = pd.DataFrame(stock.loc[stock.index[perm[:-100]]])
+stock_val = pd.DataFrame(stock.loc[stock.index[perm[-100:]]])
+labels_train = pd.DataFrame(labels.loc[labels.index[perm[:-100]]])
+labels_val = pd.DataFrame(labels.loc[labels.index[perm[-100:]]])
 cols = len(stock.columns)
 
-rows = history_train.shape[0]
-hours_back = 10 # how many hours to look back to predict the future
-N = 200 # how many samples
 # we want to predict the decision to buy, hold, or sell on the current day to maximize profits
 D_in = cols
-H = 100 # hidden layers
+H = 10 # hidden layers
 D_out = 3 # predict buy, hold, sell classes
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -62,39 +72,44 @@ print('device = ',device)
 model = torch.nn.Sequential(
         #torch.nn.BatchNorm1d(D_in),
         torch.nn.Linear(D_in,H),
-        torch.nn.ReLU(),
+        #torch.nn.Dropout(0.2),
+        #torch.nn.ReLU(),
+        torch.nn.Hardswish(),
+        #torch.nn.LeakyReLU(),
         #torch.nn.BatchNorm1d(H),
         #torch.nn.Linear(H,H),
+        #torch.nn.Dropout(0.2),
         #torch.nn.ReLU(),
+        #torch.nn.LeakyReLU(),
         #torch.nn.BatchNorm1d(H),
         #torch.nn.Linear(H,H),
+        #torch.nn.Dropout(0.2),
         #torch.nn.ReLU(),
         #torch.nn.BatchNorm1d(H),
-        torch.nn.Linear(H,D_out,bias=True),
+        torch.nn.Linear(H,D_out,bias=False),
         #torch.nn.Sigmoid()
         #torch.nn.Linear(D_in,D_out,bias=True),
         )
 
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+loss_fn = torch.nn.CrossEntropyLoss(weight=torch.Tensor([1.0,1.0,1.0]).to(device=device,dtype=torch.float))
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+#optimizer = torch.optim.SGD(model.parameters(),lr=0.01,momentum=0.9)
 
 # send to GPU
-historygpu = torch.Tensor(stock[10:].values).to(device=device,dtype=torch.float)
+historygpu = torch.Tensor(stock_train.values).to(device=device,dtype=torch.float)
 # normalize on GPU
 historygpumean = historygpu.mean(dim=0)
 historygpustd = historygpu.std(dim=0)
 historygpu = (historygpu - historygpumean)/historygpustd
-y = torch.Tensor(labels_train[10:]['class'].values).to(device=device,dtype=torch.long)
+y = torch.Tensor(labels_train['class'].values).to(device=device,dtype=torch.long)
+#print(historygpu,y)
 #print(historygpumean)
 model.to(device)
 best_model = copy.deepcopy(model)
 best_loss = 1.0
 # label the data
-for e in range(10000):
-    xin = historygpu + (torch.randn(historygpu.shape,dtype=historygpu.dtype,device=device))/20.0
-    #y = torch.stack([historygpu[ti+hours_back,3] for ti in range(t,t+N)])[:,None]#).to(device=device,dtype=torch.float)
-    #y = historygpu[ti+hours_back,:] for ti in range(t,t+N)])#).to(device=device,dtype=torch.float)
-    #print(xin.shape,y.shape,xin[0,3::7],y[0])
+for e in range(30001):
+    xin = historygpu #+ (torch.randn(historygpu.shape,dtype=historygpu.dtype,device=device))/20.0 # / 20.0
     y_pred = model(xin)
     #print(y,y_pred)
 
@@ -118,31 +133,42 @@ with torch.no_grad():
     y_pred_train = best_model(xin).argmax(dim=1)
     print(y_pred_train)
     # calculate validation data
-    stock = StockDataFrame.retype(history_val)
-    stock[image_list] # calculate statistics
-    historygpu_val = torch.Tensor(stock[10:].values).to(device=devicecpu,dtype=torch.float)
+    #stock = StockDataFrame.retype(stock_val)
+    #stock[image_list] # calculate statistics
+    historygpu_val = torch.Tensor(stock_val.values).to(device=devicecpu,dtype=torch.float)
     historygpu_val = (historygpu_val - historygpumean.cpu())/historygpustd.cpu()
     xin = historygpu_val
-    y_pred_val = best_model(xin).argmax(dim=1)
+    y_pred_val = best_model(xin)
+    y_pred_val_argmax = y_pred_val.argmax(dim=1)
     #print(y_pred)
 fig,ax = plt.subplots(figsize=(4.75,3),dpi=200,tight_layout=True)
-ax.plot(history.index,history['Close'])
-for i,day in enumerate(history_train.index[10:]):
+ax.plot(history.index,history['close'])
+for i,day in enumerate(stock_train.index):
     if y_pred_train[i] == 0: # buy
-        ax.plot(day,history_train.loc[day,'close'],'g^')
+        ax.plot(day,stock_train.loc[day,'close'],'g^')
     elif (y_pred_train[i] != 0) and (y_pred_train[i] != 2): # hold
         pass
     elif y_pred_train[i] == 2: # sell
-        ax.plot(day,history_train.loc[day,'close'],'rv')
-for i,day in enumerate(history_val.index[10:]):
-    if y_pred_val[i] == 0: # buy
-        ax.plot(day,history_val.loc[day,'close'],'g^')
-    elif (y_pred_val[i] != 0) and (y_pred_val[i] != 2): # hold
+        ax.plot(day,stock_train.loc[day,'close'],'rv')
+#for i,day in enumerate(labels_train.index):
+#    if labels_train['class'][i] == 0: # buy
+#        ax.plot(day,stock_train.loc[day,'close'],'k^',ms=1)
+#    elif (labels_train['class'][i] != 0) and (labels_train['class'][i] != 2): # hold
+#        pass
+#    elif labels_train['class'][i] == 2: # sell
+#        ax.plot(day,stock_train.loc[day,'close'],'kv',ms=1)
+for i,day in enumerate(stock_val.index):
+    if y_pred_val_argmax[i] == 0: # buy
+        ax.plot(day,stock_val.loc[day,'close'],'g^')
+    elif (y_pred_val_argmax[i] != 0) and (y_pred_val_argmax[i] != 2): # hold
         pass
-    elif y_pred_val[i] == 2: # sell
-        ax.plot(day,history_val.loc[day,'close'],'rv')
-ax.axvline(history_train.index[-1],color='k')
-ax.axvline(history_val.index[10],color='k')
+    elif y_pred_val_argmax[i] == 2: # sell
+        ax.plot(day,stock_val.loc[day,'close'],'rv')
+#ax.axvline(stock_train.index[-1],color='k')
+#ax.axvline(stock_val.index,color='k')
 fig.show()
+
+yval = torch.Tensor(labels_val['class'].values).to(device=devicecpu,dtype=torch.long)
+print(loss_fn.cpu()(y_pred_val,yval).item())
 
 input('Enter to exit')
